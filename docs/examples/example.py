16953dc07b732d135a95627bd94c88fd3a15b3c7
iosefa <ipercival@gmail.com>
2025-04-18 15:46:36 +1300

    Rename example script directory and fix formatting
    Renamed 'example' directory to 'examples' for consistency. Additionally, improved code readability by adjusting line breaks and preserving the file's original newline.

--- docs/examples/example.py
+++ docs/examples/example.py
@@ -1,12 +1,15 @@
 import os
 from spectralmatch.process import global_match, local_match
+
 working_directory = os.path.dirname(os.path.abspath(__file__))
 
 # -------------------- Parameters
 vector_mask_path = working_directory + "/input/Masks.gpkg"
 
 input_folder = os.path.join(working_directory, "input")
-global_folder = os.path.join(working_directory, "output/global_match")  # This is the output of global match
+global_folder = os.path.join(
+    working_directory, "output/global_match"
+)  # This is the output of global match
 custom_mean_factor = 3  # Defualt 1; 3 often works better to 'move' the spectral mean of images closer together
 custom_std_factor = 1  # Defualt 1
 
